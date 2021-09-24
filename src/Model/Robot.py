3dcd58cda039507f2f9692f829e9206ff9e93c59
Mople <49044525+Mople@users.noreply.github.com>
2021-09-23 21:37:33 -0400

    implement ChooseAnAction and the graph representation
    Branch system not using the number of dirt cleaned

--- src/Model/Robot.py
+++ src/Model/Robot.py
@@ -1,38 +1,114 @@
 # -*- coding: utf-8 -*-
 from Model.Capteur import Capteur
-from Model.Environment import Environment
 from Model.Vertice import Vertice
 
 class Robot:
-    
-    #Recuperation du 
+
+    #Recuperation du
     def __init__(self, controller):
         self.controller = controller
         self.capteur = Capteur()
         self.env = self.capteur.scan(controller)
         self.x = 0 #
         self.y = 0 # A random
-        
-        
-    def agent_behaviour(self): 
+
+
+    def agent_behaviour(self):
         self.env = self.capteur.scan(self.controller)
         while(not(self.env.isClean())):
-            ChooseAnAction()
+            self.ObserveAndUpdate()
+            self.ChooseAnAction()
 
-            
+
     def ObserveAndUpdate(self):
-        self.env = self.capteur.scan(controller)
+        self.env = self.capteur.scan(self.controller)
 
-        
+
     def ChooseAnAction(self):
         verticeList = []
-        firstVertice = Vertice(self.x, self.y, [], [])
+        firstVertice = Vertice(self.x, self.y, [(self.x,self.y)], [], False)
         verticeList.append(firstVertice)
         end = False
-        while(not(end) and len(verticeList)>0): 
+        
+        while(not(end) and len(verticeList)>0):
+            
             # Pop la file
-            # Marquer le noeud
+            currentVertice = verticeList.pop(0)
+            
             # Trouver ces voisins
-            # Vérifier le marquage de ceux-ci
+            neighbours = self.getNeighbours(currentVertice,verticeList)
+            
             # Les ajouter à la liste
-            print("temp")
+            for neighbour in neighbours:
+                # Vérifier si l'état final est atteint
+                if (self.env.dirtNumber == len(neighbour.getRoomsCleaned())):
+                    # Si oui on stoppe la boucle while
+                    end = True
+                    # On sauvegarde le dernier noeud
+                    finalVertice = neighbour 
+                    
+                else:
+                    # Si non l'ajouter à la liste des états à explorer
+                    verticeList.append(neighbours)
+        return finalVertice
+            
+            
+            
+            
+    # Fonction pour ajouter les voisins d'une salle
+    # +1 voisin si salle sale
+    # +x voisin si salle propre, si celles ci ne sont pas visitées dans la branche
+    def addNeighbours(self,vertice,verticeList):
+        neighbourList = []
+        if self.env.getSalle(vertice.getX(), vertice.getY()).getDirt():
+            # Cas non propre
+            newVertice = Vertice (vertice.getX(),
+                                  vertice.getY(),
+                                  vertice.getPath(),
+                                  vertice.getRoomsCleaned.append((vertice.getX(),vertice.getY())),
+                                  True)
+            neighbourList.append(newVertice)
+        else:
+            # Cas propre
+            # On récupère ls coordonnées des salles adjacentes
+            listCoord = self.getNeighbourCoord(vertice.getX(),vertice.getY())
+            # On regarde si elles sont déjà parcourues dans cette branche
+            for coord in listCoord:
+                if coord in verticeList:
+                    # Salle déjà explorée dans cette branche
+                    break
+                else:
+                    # Salle non explorée dans cette branche
+                    newVertice = Vertice(coord[0],
+                                         coord[1],
+                                         vertice.getPath.append(coord),
+                                         vertice.getRoomsCleaned()
+                                         )
+                    neighbourList.append(newVertice)
+        return neighbourList
+                    
+            
+            
+    def getNeighbourCoord(x,y):
+        if x==0:
+            if y==0:
+                return [(x+1,y), (x,y+1)]
+            elif y==4:
+                return [(x+1,y), (x, y-1)]
+            else:
+                return [(x+1,y), (x, y-1), (x,y+1)]
+        elif x==4:
+            if y==0:
+                return [(x-1,y), (x,y+1)]
+            elif y==4:
+                return [(x-1,y), (x, y-1)]
+            else:
+                return [(x-1,y), (x, y-1), (x,y+1)]
+        else :
+            if y==0:
+                return [(x-1,y), (x+1,y), (x,y+1)]
+            elif y==4:
+                return [(x-1,y), (x+1,y), (x, y-1)]
+            else:
+                return [(x-1,y), (x+1,y), (x, y-1), (x,y+1)]
+            
--- src/Model/Vertice.py
+++ src/Model/Vertice.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 
 class Vertice:
-    
-    def __init__(self, roomX, roomY,listRooms, roomsCleaned):
+
+    def __init__(self,roomX, roomY,listRooms, roomsCleaned, roomState):
+        # Coordonnées de la salle
         self.x = roomX
         self.y = roomY
-        self.listRooms = listRooms
+        # Etat propre ou salle
+        self.clean = roomState
+        # List des salles visitées pour arriver dans cette salle (=chemin de la branche)
+        self.path = listRooms
+        # Liste des salles nettoyées sur le chamin ci dessus (donc dans la branche)
         self.roomsCleaned = roomsCleaned
-        
-        
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+    
+    def getState(self):
+        return self.clean
+
+    def getPath(self):
+        return self.path
+
+    def getRoomsCleaned(self):
+        return self.roomsCleaned
